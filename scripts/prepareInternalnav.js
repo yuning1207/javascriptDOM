76b6bcfe86d76990d66ba2a4c8b8bf6006f0a869
yuning1207 <1578517699@qq.com>
2018-04-25 11:34:27 +0800

    add

--- scripts/prepareInternalnav.js
+++ scripts/prepareInternalnav.js
@@ -1,25 +1,22 @@
-function prepareInternalnav()
-{
-    if(!document.getElementsByTagName)
+function prepareInternalnav() {
+    if (!document.getElementsByTagName)
         return false;
-    if(!document.getElementById)
+    if (!document.getElementById)
         return false;
-    var articles=document.getElementsByTagName("article");
-    if(articles.length==0)
+    var articles = document.getElementsByTagName("article");
+    if (articles.length == 0)
         return false;
-    var navs=articles[0].getElementsByTagName("nav");
-    if(navs.length==0)
+    var navs = articles[0].getElementsByTagName("nav");
+    if (navs.length == 0)
         return false;
-    var links=navs[0].getElementsByTagName("a");
-    for(var i=0;i<links.length;i++)
-    {
-        vat sectionId=links[i].getAttribute("href").split("#")[1];
-        if(!document.getElementById(sectionId))
+    var links = navs[0].getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        var sectionId = links[i].getAttribute("href").split("#")[1];
+        if (!document.getElementById(sectionId))
             continue;
-        document.getElementById(sectionId).style.display="none";
-        links[i].destination=sectionId;
-        links[i].onclick=function()
-        {
+        document.getElementById(sectionId).style.display = "none";
+        links[i].destination = sectionId;
+        links[i].onclick = function() {
             showSection(this.destination);
             return false;
         }
